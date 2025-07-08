6e78043d484557508eb4e9b8f1747800e7e6992d
Graciliano Barroso <grico484@gmail.com>
2025-07-08 10:39:33 -0400

    Incuido a aula do dia 13 ao projeto

--- nestJS-aulas/primeira_semana/projeto-tarefas/src/app.module.ts
+++ nestJS-aulas/primeira_semana/projeto-tarefas/src/app.module.ts
@@ -1,14 +1,33 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TarefasModule } from './tarefas/tarefas.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TarefasModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna disponível em toda a aplicação
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true, // carrega entidades automaticamente
+        synchronize: true, // ⚠️ usar apenas em desenvolvimento!
+      }),
+    }),
+    TarefasModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes("tarefas") // aplica somente às rotas de /tarefas
+    consumer.apply(LoggerMiddleware).forRoutes('tarefas'); // aplica somente às rotas de /tarefas
   }
 }
--- nestJS-aulas/primeira_semana/projeto-tarefas/src/main.ts
+++ nestJS-aulas/primeira_semana/projeto-tarefas/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv'
+
+// Carrega as variáveis do .env antes de tudo
+dotenv.config();
 
 async function bootstrap() {
   console.log('Ambiente atual:', process.env.NODE_ENV || 'desenvolvimento');
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+  const port = process.env.PORT || 3000;
   await app.listen(3000);
+
+  console.log(`Servidor rodando na porta ${port}`);
 }
 bootstrap();
